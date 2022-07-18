b19e955ee46e44d89ec4e2820f45438e5f37517f
Ivan Vecera <ivan@cera.cz>
2022-07-18 19:32:13 +0200

    sdm710-common: Add shim for missing symbol in lib-imsvideocodec.so
    Commit 992496bd2e ("Allow creating child surfaces from
    BlastBufferQueue") added 3rd parameter to Surface::Surface
    constructor that causes an unresolved symbol in existing blobs.
    
    Provide a shim library with implementation of older Surface
    constructor for lib-imsvidcodec.so that references the mentioned
    symbol. Class constructor is a function with thiscall calling
    convention so to preserve ABI we have to provide a C-function with
    pointer as first parameter (that is hidden in C++) and return that
    pointer from the function.
    
    Resolves:
    E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/system_ext/lib64/lib-imsvideocodec.so"
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I91abeabb07cac8553ee548e507a949e929590995

--- sdm710.mk
+++ sdm710.mk
@@ -356,6 +356,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
+    lib-imsvtshim:64 \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
