2a36b76e64eeb23cb94faab21fa0ba061370fdcc
Ivan Vecera <ivan@cera.cz>
2022-08-17 18:28:35 +0200

    sdm710-common: Update NFC config files
    With current NFC configuration the payments using GooglePay is
    not functional.
    
    * Add missing android.hardware.nfc.hcef.xml
    * Add libnfc-nci.conf borrowed from dipper
    * Replace libnfc-nxp.conf by one from dipper and adjusted for
      Grus and Pyxis
    
    Tested: Successful payment using GooglePay
    Change-Id: I53edb970a4a7ac4f791ffad80e18926a0325c23c

--- sdm710.mk
+++ sdm710.mk
@@ -248,11 +248,13 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
