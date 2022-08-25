cfd74c82d8d5961d435d57a499c608359c9d1b8d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-25 14:56:02 +0200

    sdm710-common: Enable external USB camera support
    Change-Id: I8a9f1c8fbd566a0ff58c43559db347de5e060e6d

--- sdm710.mk
+++ sdm710.mk
@@ -100,7 +100,11 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-external-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
