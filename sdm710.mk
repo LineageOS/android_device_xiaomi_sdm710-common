303cd4d49b90726fb06818d57a1bcfe2e73d795f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-04 01:27:22 +0200

    sdm710-common: Move headset keylayout to common
    Change-Id: Id24364821cebbee2c6bb8bb494d4f905a418ff2f

--- sdm710.mk
+++ sdm710.mk
@@ -201,6 +201,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     IFAAService
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/sdm670-mtp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm670-mtp-snd-card_Button_Jack.kl
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
