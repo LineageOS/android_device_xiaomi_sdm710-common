9e81a9afbe2a91c658f4d7291b912e77bcc9dcf4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-05 17:07:12 +0200

    sdm710-common: sepolicy: Label logdump partition in qcom sepolicy
    Change-Id: I9c0678257ad21480581b30431dfcf3a5cd215bfd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,6 +129,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2021-04-01
 
 # Sepolicy
+TARGET_USES_LOGDUMP_AS_METADATA := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
