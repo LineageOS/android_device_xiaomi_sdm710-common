1b61c7b216dd2c368a251fe55a43e41d6499ef9d
Edwin Moquete <edwinmmoquete@gmail.com>
2022-09-06 13:40:45 +0000

    sdm710-common: Update WFD system stack from T blobs
    Change-Id: I3035aeafba00315c0897b913015d562578c0ce69

--- sdm710.mk
+++ sdm710.mk
@@ -132,7 +132,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     libqdMetaData \
+    libqdMetaData.system \
     libtinyxml \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
