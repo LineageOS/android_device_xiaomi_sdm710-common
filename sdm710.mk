a630a9519543fd3d446d06cc4e21dc5cdbd34419
Ivan Vecera <ivan@cera.cz>
2022-09-13 17:08:13 +0200

    sdm710-common: Import XiaomiParts from sdm845-common
    * As of LineageOS/android_device_xiaomi_sdm845-common/commit/0125b10db7d4
    * Only Doze-related stuff, other stuff will be added maybe later
    
    Change-Id: Icdd4191f4a9cd584062f806f275a5debad118bc1

--- sdm710.mk
+++ sdm710.mk
@@ -120,6 +120,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
