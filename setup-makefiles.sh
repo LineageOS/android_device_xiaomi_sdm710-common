e4a488819053a2de2ef46851f8a0f5f951a9ea38
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-21 17:38:48 +0800

    sdm710-common: Unify pyxis and vela as pyxis_vela
    Well, the community prefers this name
    
    Change-Id: I5d8eac0443ede640568ae031e91eeca357a4976b

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "grus pyxis xmsirius vela"
+write_headers "grus pyxis xmsirius vela pyxis_vela"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
