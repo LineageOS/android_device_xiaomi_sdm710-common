92b9d8772d24bbe374d4fc38e4cdb0fa6b2b90b4
LuK1337 <priv.luk@gmail.com>
2023-10-21 10:07:10 +0200

    sdm710-common: Update libstdc++.vendor target name
    Change-Id: I9fb48d50df7b8b74d19bfb05b3d9c2ca169bf3e2

--- sdm710.mk
+++ sdm710.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libmegface \
-    libstdc++.vendor \
+    libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
