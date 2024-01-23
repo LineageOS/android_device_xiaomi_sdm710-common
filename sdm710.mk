f72edbd2b7bd55164ea97cc4d90034730b541e91
Ivan Vecera <ivan@cera.cz>
2024-01-23 11:33:51 +0100

    sdm710-common: Build libprotobuf-cpp 3.9.1
    Build libprotobuf-cpp-{full,lite}-3.9.1 vendor libraries that are
    required for various blobs.
    
    Change-Id: I3f8fec49db5f78f9325190c54766e3b455d2fe05

--- sdm710.mk
+++ sdm710.mk
@@ -326,6 +326,11 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
