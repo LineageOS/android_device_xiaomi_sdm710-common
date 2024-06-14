d90bd0864369ca0bb4699f10a4a0c75c564d500e
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:50:24 +0300

    sdm710-common: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,6 +161,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
