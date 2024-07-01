LineageOS/android_device_xiaomi_sdm710-common

commit 1b61c7b216dd2c368a251fe55a43e41d6499ef9d

sdm710-common: Update WFD system stack from T blobs
Change-Id: I3035aeafba00315c0897b913015d562578c0ce69

sdm710.mk
@@ -132,7 +132,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     libqdMetaData \
+    libqdMetaData.system \
     libtinyxml \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \

commit 1c4cc78301b8f7dde68d71e4656e375ff7c63918

sdm710-common: Add missing permissions for WFD
Fix missing WFD permissions file after the WFD update introduced by
commit 1b61c7b21 ("sdm710-common: Update WFD system stack from T
blobs").

Fixes: 1b61c7b21 ("sdm710-common: Update WFD system stack from T blobs")
Change-Id: Ib8e4619549a40d72e157f9529d84349a52f1aa4f

permissions/wfd-system-ext-privapp-permissions-qti.xml
@@ -1,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!--
+  Copyright (c) 2020 Qualcomm Technologies, Inc.
+  All Rights Reserved.
+  Confidential and Proprietary - Qualcomm Technologies, Inc.
+-->
+<permissions>
+    <privapp-permissions package="com.qualcomm.wfd.service">
+        <permission name="android.permission.WRITE_MEDIA_STORAGE"/>
+    </privapp-permissions>
+</permissions>

sdm710.mk
@@ -447,6 +447,9 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/wfd-system-ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/wfd-system-ext-privapp-permissions-qti.xml
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
 