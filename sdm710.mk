bd7350dee187dd58a03338018c92cb3baa6929ee
Ivan Vecera <ivan@cera.cz>
2024-07-01 17:14:43 +0200

    sdm710-common: Import media codecs files
    We need to remove references to software OMX codecs so we cannot ship it
    via proprietary-files.txt
    
    Change-Id: Ib9d689b0f9b6d91b1bfe7b38adf3b55ab82cfbb0

--- sdm710.mk
+++ sdm710.mk
@@ -261,6 +261,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Mlipay
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.1.vendor \
