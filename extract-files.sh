911c9b2803a54545bbb521e200ef25998f008752
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-24 22:11:39 +0800

    sdm710-common: extract-files: Check before appending to seccomp policy
    Change-Id: If662c2eac63052fdbdc6ec8a0ed1e79c010ebc47

--- extract-files.sh
+++ extract-files.sh
@@ -30,7 +30,7 @@ function blob_fixup() {
             grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
             ;;
         vendor/etc/seccomp_policy/vendor.qti.hardware.dsp.policy)
-            echo 'madvise: 1' >> ${2}
+            grep -q 'madvise: ' "${2}" || echo 'madvise: 1' >> "${2}"
             ;;
         vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
             if ! "${PATCHELF}" --print-needed "${2}" | grep -q "libcrypto_shim.so"; then
