c59af51ec35f4b04cae11ab31b88248b05f20212
Yumi Yukimura <me.cafebabe@gmail.com>
2025-09-23 19:12:50 +0800

    sdm710-common: Switch to common LiveDisplay sysfs AIDL HAL
    Change-Id: Ia54ef9b05ead1385a43566aa9036658e2e1dddc3

--- sdm710.mk
+++ sdm710.mk
@@ -186,7 +186,10 @@ $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.sdm710
+    vendor.lineage.livedisplay-service.sysfs
+
+$(call soong_config_set,livedisplay_sysfs,enable_af,true)
+$(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
 # Media configs
 PRODUCT_COPY_FILES += \
